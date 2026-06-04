66f5320f5926ad70b4f4a9059a73128034eed482
DEBTULYA2005 <debtulya463@gmail.com>
2026-06-04 16:40:29 +0530

    add update-delete url

--- manage/settings.py
+++ manage/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-te!xi)2mh8@15%3r1%j24-6_#kd1l@d-_4=r6gwmu)l!$a3nj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['freemanage.onrender.com', "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
